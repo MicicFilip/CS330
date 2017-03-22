6250b378620cc58aa376412befd236da4902499a
MicicFilip <micic.filip@gmail.com>
2017-03-22 11:59:45 +0100

    V04, V05

---
+++ app/src/main/java/com/example/micic/cs330_v04/MainActivity.java
@@ -0,0 +1,198 @@
+package com.example.micic.cs330_v04;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void idiNaDruguAktivnost(View v){
+        Intent drugiPage = new Intent(this, SecondActivity.class);
+        startActivity(drugiPage);
+    }
+
+    public void btnSaved_clicked(View view) {
+        DisplayToast("Kliknuli ste na dugme Save");
+    }
+
+    static int progress;
+    ProgressBar progressBar;
+    int progressStatus = 0;
+    Handler handler = new Handler();
+
+    TimePicker timePicker;
+    DatePicker datePicker;
+    int hour, minute;
+    int yr, month, day;
+    static final int TIME_DIALOG_ID = 0;
+    static final int DATE_DIALOG_ID = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progress = 0;
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setMax(200);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus = doSomeWork();
+
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                        }
+                    });
+                }
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
+            }
+
+            private int doSomeWork() {
+                try {
+                    Thread.sleep(50);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return ++progress;
+            }
+
+
+        }).start();
+
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        timePicker.setIs24HourView(true);
+
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+
+        Calendar today = Calendar.getInstance();
+        yr = today.get(Calendar.YEAR);
+        month = today.get(Calendar.MONTH);
+        day = today.get(Calendar.DAY_OF_MONTH);
+        showDialog(DATE_DIALOG_ID);
+
+
+        Button btnOpen = (Button) findViewById(R.id.btnOpen);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DisplayToast("Kliknuli ste dugme open");
+            }
+        });
+
+
+        CheckBox checkBox = (CheckBox) findViewById(R.id.chkAutosave);
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (((CheckBox) v).isChecked()) {
+                    DisplayToast("Checkbox je cekiran");
+                } else {
+                    DisplayToast("Checkbox je odcekiran");
+                }
+            }
+        });
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.rdbGp1);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton rb1 = (RadioButton) findViewById(R.id.rdb1);
+                if (rb1.isChecked()) {
+                    DisplayToast("Option 1 je cekiran");
+                } else {
+                    DisplayToast("Option 2 je necekiran");
+                }
+            }
+        });
+
+        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggle1);
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (((ToggleButton) v).isChecked()) {
+                    DisplayToast("Toggle je ukljucen");
+                } else {
+                    DisplayToast("Toggle je iskljucen");
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case TIME_DIALOG_ID:
+                return new TimePickerDialog(this, mTimeListener, hour, minute, false);
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this, mDateListener, yr, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener mDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            yr = year;
+            month = monthOfYear;
+            day = dayOfMonth;
+            Toast.makeText(getBaseContext(), "Izabrali ste : " + (month + 1) + "/" + day + "/" + year, Toast.LENGTH_SHORT).show();
+        }
+
+
+    };
+
+    private TimePickerDialog.OnTimeSetListener mTimeListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minuteOfHour) {
+            hour = hourOfDay;
+            minute = minuteOfHour;
+
+            SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm aa");
+            Date date = new Date(0, 0, 0, hour, minute);
+            String strDate = timeFormat.format(date);
+
+            Toast.makeText(getBaseContext(), "Izabrali ste " + strDate, Toast.LENGTH_SHORT);
+        }
+    };
+
+    public void onClick(View view) {
+        Toast.makeText(getBaseContext(), "Izabrani datum: " + (datePicker.getMonth() + 1) + "/" + datePicker.getDayOfMonth() + "/" + datePicker.getYear() + "\n" + "Izabrano vreme: " + timePicker.getCurrentHour() + ":" + timePicker.getCurrentMinute(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void DisplayToast(String msg) {
+        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+}
---
+++ app/src/main/java/com/example/micic/cs330_v04/SecondActivity.java
@@ -0,0 +1,223 @@
+package com.example.micic.cs330_v04;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.view.animation.AnimationUtils;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.Gallery;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher.ViewFactory;
+
+
+/**
+ * Created by Micic on 22-Mar-17.
+ */
+
+public class SecondActivity extends AppCompatActivity implements ViewFactory {
+
+    //---slike za prikazivanje---
+    Integer[] imageIDs = {
+            R.drawable.pic1,
+            R.drawable.pic2,
+            R.drawable.pic3,
+            R.drawable.pic4
+
+    };
+
+    private ImageSwitcher imageSwitcher;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_main);
+
+        WebView wv = (WebView) findViewById(R.id.webview1);
+        WebSettings webSettings = wv.getSettings();
+        webSettings.setBuiltInZoomControls(true);
+        wv.setWebViewClient(new Callback());
+        wv.loadUrl("https://www.metropolitan.ac.rs");
+
+        Button btn = (Button) findViewById(R.id.btn1);
+        btn.setOnCreateContextMenuListener(this);
+
+        imageSwitcher = (ImageSwitcher) findViewById(R.id.switcher1);
+        imageSwitcher.setFactory(this);
+
+        imageSwitcher.setInAnimation(AnimationUtils.loadAnimation(this,
+                android.R.anim.slide_in_left));
+        imageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(this,
+                android.R.anim.slide_out_right));
+        Gallery gallery = (Gallery) findViewById(R.id.gallery1);
+        gallery.setAdapter(new ImageAdapter(this));
+        gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent,
+                                    View v, int position, long id) {
+                imageSwitcher.setImageResource(imageIDs[position]);
+            }
+        });
+
+
+    }
+
+    private class Callback extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return(false);
+        }
+    }
+
+    public View makeView() {
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundColor(0xFF000000);
+        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        imageView.setLayoutParams(new
+                ImageSwitcher.LayoutParams(
+                LayoutParams.FILL_PARENT,
+                LayoutParams.FILL_PARENT));
+        return imageView;
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        Context context;
+        int itemBackground;
+
+        public ImageAdapter(Context c) {
+            context = c;
+//---podešava stil---
+            TypedArray a = obtainStyledAttributes(R.styleable.Gallery1);
+            itemBackground = a.getResourceId(
+                    R.styleable.Gallery1_android_galleryItemBackground, 0);
+            a.recycle();
+        }
+
+        //---vraća broj slika---
+        public int getCount() {
+            return imageIDs.length;
+        }
+
+        //---vraća stavku---
+        public Object getItem(int position) {
+            return position;
+        }
+
+        //---vraća ID of stavke---
+        public long getItemId(int position) {
+            return position;
+        }
+
+        //---vraća ImageView pogled---
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                imageView = new ImageView(context);
+                imageView.setImageResource(imageIDs[position]);
+                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                imageView.setLayoutParams(new Gallery.LayoutParams(150, 120));
+            } else {
+                imageView = (ImageView) convertView;
+            }
+            imageView.setBackgroundResource(itemBackground);
+            return imageView;
+        }
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        CreateMenu(menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        return MenuChoice(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        CreateMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return MenuChoice(item);
+    }
+
+
+    public void CreateMenu(Menu menu) {
+        menu.setQwertyMode(true);
+        MenuItem mnu1 = menu.add(0, 0, 0, "CS101");
+        {
+            mnu1.setAlphabeticShortcut('a');
+            mnu1.setIcon(R.mipmap.ic_launcher);
+        }
+        MenuItem mnu2 = menu.add(0, 1, 1, "CS102");
+        {
+            mnu2.setAlphabeticShortcut('b');
+            mnu2.setIcon(R.mipmap.ic_launcher);
+        }
+        MenuItem mnu3 = menu.add(0, 2, 2, "CS330");
+        {
+            mnu3.setAlphabeticShortcut('c');
+            mnu3.setIcon(R.mipmap.ic_launcher);
+        }
+        MenuItem mnu4 = menu.add(0, 3, 3, "IT355");
+        {
+            mnu4.setAlphabeticShortcut('d');
+        }
+        menu.add(0, 4, 4, "IT205");
+        menu.add(0, 5, 5, "MAT101");
+        menu.add(0, 6, 6, "CS103");
+    }
+
+    private boolean MenuChoice(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                Toast.makeText(this, "Izabrali ste predmet CS101",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case 1:
+                Toast.makeText(this, "Izabrali ste predmet CS102",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case 2:
+                Toast.makeText(this, "Izabrali ste predmet CS330",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case 3:
+                Toast.makeText(this, "Izabrali ste predmet IT355",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case 4:
+                Toast.makeText(this, "Izabrali ste predmet IT205",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case 5:
+                Toast.makeText(this, "Izabrali ste predmet MAT101",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case 6:
+                Toast.makeText(this, "Izabrali ste predmet CS103",
+                        Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return false;
+    }
+}
