d4c7128ff31f47a69138cd05d71c85d465814460
MicicFilip <micic.filip@gmail.com>
2017-03-29 13:40:45 +0200

    db problems

---
+++ app/src/main/java/com/example/micic/cs330_v04/DbAdapter.java
@@ -0,0 +1,106 @@
+package com.example.micic.cs330_v04;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Micic on 29-Mar-17.
+ */
+
+public class DbAdapter {
+
+    static final String KEY_ID = "id";
+    static final String KEY_NAME = "username";
+    static final String KEY_EMAIL = "email";
+    static final String TAG = "DbAdapter";
+
+    static final String DATABASE_NAME = "cs330db";
+    static final String DATABASE_TABLE = "user";
+    static final int DATABASE_VERSION = 3;
+
+    static final String DATABASE_CREATE = "CREATE TABLE user (id integer primary key autoincrement, " + "username text not null, email text not null";
+
+    final Context context;
+    DatabaseHelper DBHelper;
+    SQLiteDatabase db;
+
+    public DbAdapter(Context context) {
+        this.context = context;
+        DBHelper = new DatabaseHelper(context);
+    }
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            try {
+                db.execSQL(DATABASE_CREATE);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Ažuriranje verzije baze podataka sa " + oldVersion + " na verziju " + newVersion + ", a to će uništiti postojeće podatke");
+            db.execSQL("DROP TABLE IF EXISTS user");
+            onCreate(db);
+        }
+    }
+
+    //---otvaranje baze podataka--
+    public DbAdapter open() throws SQLException {
+        db = DBHelper.getWritableDatabase();
+        return this;
+    }
+
+    //---zatvaranje baze podataka
+    public void close() {
+        DBHelper.close();
+    }
+
+    public long insertContact(String username, String email) {
+        ContentValues initalValues = new ContentValues();
+        initalValues.put(KEY_NAME, username);
+        initalValues.put(KEY_EMAIL, email);
+
+        return db.insert(DATABASE_TABLE, null, initalValues);
+    }
+
+    //---brisanje konkretnog kontakta
+    public boolean deleteContact(long rowId) {
+        return db.delete(DATABASE_TABLE, KEY_ID + "=" + rowId, null) > 0;
+    }
+
+    // listanje svih kontakta iz baze
+    public Cursor getAllContacts() {
+        return db.query(DATABASE_TABLE, new String[]{KEY_ID, KEY_NAME, KEY_EMAIL}, null, null, null, null, null);
+    }
+
+    // get-ovanje jednog kontakta
+    public Cursor getContact(long rowId) throws SQLException {
+        Cursor mCursor = db.query(true, DATABASE_TABLE, new String[]{KEY_ID, KEY_NAME, KEY_EMAIL}, KEY_ID + "=" + rowId, null, null, null, null, null);
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+
+    // upate-ovanje kontakta
+    public boolean updateContact(long rowId, String username, String email) {
+        ContentValues args = new ContentValues();
+        args.put(KEY_NAME, username);
+        args.put(KEY_EMAIL, email);
+        return db.update(DATABASE_TABLE, args, KEY_ID + "=" + rowId, null) > 0;
+    }
+
+
+}
--- app/src/main/java/com/example/micic/cs330_v04/V06Activity.java
+++ app/src/main/java/com/example/micic/cs330_v04/V06Activity.java
@@ -1,8 +1,19 @@
 package com.example.micic.cs330_v04;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created by Micic on 29-Mar-17.
@@ -14,5 +25,102 @@ public class V06Activity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_v06);
+
+        final DbAdapter db = new DbAdapter(this);
+
+        Button addContacts = (Button) findViewById(R.id.addContacts);
+        addContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.open();
+                db.insertContact("filip", "micic@mail.com");
+                db.insertContact("pera", "p@gmail.com");
+                db.insertContact("maja", "m@gmail.com");
+                db.close();
+            }
+        });
+
+        Button getContacts = (Button) findViewById(R.id.getContacts);
+        getContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.open();
+                Cursor c = db.getAllContacts();
+                if (c.moveToFirst()) {
+                    do {
+                        DisplayContact(c);
+                    } while (c.moveToNext());
+                }
+                db.close();
+            }
+        });
+
+        Button getOneContact = (Button) findViewById(R.id.getOneContact);
+        getOneContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.open();
+                Cursor d = db.getContact(2);
+                if (d.moveToFirst()) {
+                    DisplayContact(d);
+                } else {
+                    DisplayToast("Nije pronadjen kontakt");
+                    ;
+                }
+                db.close();
+            }
+        });
+
+        Button updateContacts = (Button) findViewById(R.id.updateContact);
+        updateContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.open();
+                if (db.updateContact(2, "Milan", "mil@gmail.com")) {
+                    DisplayToast("Uspešno ažuriranje");
+                } else {
+                    DisplayToast("Greška u ažuriranju");
+                }
+                db.close();
+            }
+        });
+
+        try {
+            String destPath = "/data/data/" + getPackageName() + "/databases";
+
+            System.out.println(destPath);
+
+            File f = new File(destPath);
+            if (!f.exists()) {
+                f.mkdirs();
+                f.createNewFile();
+                CopyDB(getBaseContext().getAssets().open("database"), new FileOutputStream(destPath + "/Database"));
+
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public void CopyDB(InputStream inputStream, OutputStream outputStream) throws IOException {
+        byte[] buffer = new byte[1024];
+        int lenght;
+        while ((lenght = inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer, 0, lenght);
+        }
+        inputStream.close();
+        outputStream.close();
+    }
+
+    public void DisplayContact(Cursor c) {
+        Toast.makeText(this, "id: " + c.getString(0) + "\n" + "ime: " + c.getString(1) + "\n" + "Email:  " + c.getString(2), Toast.LENGTH_LONG).show();
+    }
+
+    private void DisplayToast(String msg) {
+        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
 }
